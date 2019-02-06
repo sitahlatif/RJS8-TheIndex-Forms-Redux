e8f080da5e755be3c158953b6bc242d6d6844cec
Octowl <alsaff1987@gmail.com>
2019-02-06 15:23:30 +0300

    bind author form to state

--- src/AuthorForm.js
+++ src/AuthorForm.js
@@ -15,6 +15,9 @@ class AuthorForm extends Component {
     if (this.props.errors.length) this.props.resetErrors();
   }
 
+  textChangeHandler = event =>
+    this.setState({ [event.target.name]: event.target.value });
+
   submitAuthor = event => {
     event.preventDefault();
     this.props.postAuthor(this.state, this.props.closeModal);
@@ -37,19 +40,34 @@ class AuthorForm extends Component {
             <div className="input-group-prepend">
               <span className="input-group-text">First Name</span>
             </div>
-            <input type="text" className="form-control" name="first_name" />
+            <input
+              type="text"
+              className="form-control"
+              name="first_name"
+              onChange={this.textChangeHandler}
+            />
           </div>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span className="input-group-text">Last Name</span>
             </div>
-            <input type="text" className="form-control" name="last_name" />
+            <input
+              type="text"
+              className="form-control"
+              name="last_name"
+              onChange={this.textChangeHandler}
+            />
           </div>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span className="input-group-text">Image URL</span>
             </div>
-            <input type="text" className="form-control" name="imageUrl" />
+            <input
+              type="text"
+              className="form-control"
+              name="imageUrl"
+              onChange={this.textChangeHandler}
+            />
           </div>
           <input type="submit" />
         </form>
